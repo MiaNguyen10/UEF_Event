c97e5480156d441d7d4ef46007c032bff5b62435
NguyenThao1904 <thanhthao.976431@gmail.com>
2021-12-20 19:36:26 +0700

    Fix css sidebar

--- quan_ly_su_kien/src/components/Sidebar/MenuItems.js
+++ quan_ly_su_kien/src/components/Sidebar/MenuItems.js
@@ -1,52 +1,58 @@
-import React from 'react';
-import * as RiIcons from 'react-icons/ri';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
-import * as GiIcons from 'react-icons/gi';
-import * as GrIcons from 'react-icons/gr';
+// import React from 'react';
+// import * as RiIcons from 'react-icons/ri';
+// import * as AiIcons from 'react-icons/ai';
+// import * as IoIcons from 'react-icons/io';
+// import * as GiIcons from 'react-icons/gi';
+// import * as GrIcons from 'react-icons/gr';
 import logo from "../../asset/img/logo.png";
+import past_event from "../../asset/img/past_even.png";
+import love_event from "../../asset/img/love_event.png";
+import language from "../../asset/img/language.png";
+import theme from "../../asset/img/theme.png";
+import setting from "../../asset/img/setting.png";
+import logout from "../../asset/img/logout.png";
 
 export const MenuItems = [
   {
     title: 'Tên đăng nhập',
     path: '/',
-    icon: logo,
-    cName: 'nav-username'
+    icon: logo, //fix later
+    cName: 'nav-text nav-username'
   },
   {
     title: 'Sự kiện đã qua',
     path: '/recently',
-    icon: <GiIcons.GiBackwardTime />,
+    icon: past_event,
     cName: 'nav-text'
   },
   {
     title: 'Sự kiện yêu thích',
     path: '/loveevent',
-    icon: <AiIcons.AiFillHeart />,
+    icon: love_event,
     cName: 'nav-text'
   },
   {
     title: 'Ngôn ngữ',
     path: '/language',
-    icon: <GrIcons.GrLanguage />,
+    icon: language,
     cName: 'nav-text'
   },
   {
     title: 'Giao diện',
     path: '/theme',
-    icon: <RiIcons.RiBrush3Line />,
+    icon: theme,
     cName: 'nav-text'
   },
   {
     title: 'Cài đặt',
     path: '/settings',
-    icon: <AiIcons.AiFillSetting />,
+    icon: setting,
     cName: 'nav-text'
   },
   {
     title: 'Log out',
     path: '/logout',
-    icon: <IoIcons.IoMdExit />,
+    icon: logout,
     cName: 'nav-text'
   }
 ];
