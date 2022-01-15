7f44aebba2191fc42b1e7332a4370d35ed1dcdf5
NguyenThao1904 <thanhthao.976431@gmail.com>
2022-01-15 14:56:57 +0700

    Fix bug log out

--- quan_ly_su_kien/src/pages/Login/Logout.js
+++ quan_ly_su_kien/src/pages/Login/Logout.js
@@ -1,13 +1,41 @@
 import React from "react";
 import Cookies from "universal-cookie";
 import { Navigate } from 'react-router-dom';
+import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
+import Swal from 'sweetalert2';
 
 const Logout = () => {
-    const cookies = new Cookies()
-    cookies.remove('authToken', { path: '/' });
-    // window.location.reload();
-    return <Navigate to="/" />
-    
+    const lang = localStorage.getItem('lang');
+    i18next.changeLanguage(lang);
+    const { t, i18n } = useTranslation();
+    return (
+        Swal.fire({
+            title: t('Popup.ques_logout'),
+            showCancelButton: true,
+            confirmButtonText: t('Popup.yes'),
+            cancelButtonText: t('Popup.no')
+          }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                    const cookies = new Cookies();
+                    cookies.remove('authToken', { path: '/' });    
+                    <Navigate to="/" />           
+                    Swal.fire({
+                        title: t('Popup.come_home1') + '<a href="/">' + t('Popup.come_home2') + '</a>' + t('Popup.logout_success'),
+                        icon: 'info',
+                        showConfirmButton: false});
+                }else{
+                    Swal.fire({
+                    title: t('Popup.come_home1') + '<a href="/">' + t('Popup.come_home2') + '</a>' + t('Popup.come_home3'),
+                    icon: 'info',
+                    showConfirmButton: false});
+                }
+                
+            }
+        )
+    )
+
 };
 
 export default Logout;
